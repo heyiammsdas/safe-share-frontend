cf005771d318ce3b61acfbc79135cb6b6c1d7a15
MadhusudanDas <msdas7476@gmail.com>
2025-12-05 11:43:08 +0530

    env file included

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 interface User {
   _id: string;
@@ -49,6 +49,7 @@ export default function App() {
       setNoteId(match[1]);
       setCurrentView("viewNote");
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleLogout = () => {
